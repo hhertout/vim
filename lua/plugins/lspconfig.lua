9127793eb283da727c1f783bf3f0f574025c341f
hhertout <hugues.hertout@gmail.com>
2024-06-24 18:30:06 +0200

    feat: adding support for glsl files - lsp + syntx highlighting

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -30,6 +30,7 @@ return {
         csharp_ls = {},
         dockerls = {},
         docker_compose_language_service = {},
+        glsl_analyzer = {},
       },
       -- you can do any additional lsp server setup here
       -- return true if you don't want this server to be setup with lspconfig
